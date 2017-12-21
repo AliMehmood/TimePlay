981921170f0062f0dd7bd74abea1940dd2f651b8
Ali Mehmood <alimehmood@Alis-Mac.local>
2017-12-21 23:05:08 +0500

    new changes

--- ShozabBhai/ViewController.swift
+++ ShozabBhai/ViewController.swift
@@ -10,55 +10,95 @@ import UIKit
 
 class ViewController: UIViewController {
     var timerObject = Timer()
-    var counter = 0
+    var timerObject2 = Timer()
+    var counter = 30
+    var counter2 = 30
     
-    @IBOutlet weak var playbutton: UIBarButtonItem!
-    @IBOutlet weak var directionImage: UIImageView!
-    @IBOutlet weak var timerLbl: UILabel!
-    @IBOutlet weak var clickToPlay: UILabel!
+    @IBOutlet weak var playButton2: UIButton!
+    @IBOutlet weak var playButton1: UIButton!
+    @IBOutlet weak var color1: UIView!
+    @IBOutlet weak var color2: UIView!
     var random =  UInt32()
+    var random2 =  UInt32()
 
-    override func viewDidLoad() {
-        self.timerLbl.isHidden = false
+    override func  viewDidLoad() {
+//        self.timerLbl.isHidden = false
+//        self.color1.isHidden = false
+//        self.color2.isHidden = false
+        self.color1.backgroundColor = UIColor.lightGray
+        self.color2.backgroundColor = UIColor.lightGray
     }
-    @IBAction func startTimer(_ sender: UIBarButtonItem) {
-        counter = 0
-        self.timerLbl.isHidden = false
-        timerObject = Timer.scheduledTimer(timeInterval: 1, target: self, selector: Selector("timer"), userInfo: nil, repeats: true)
-        random = arc4random_uniform(80) + 10
+    @IBAction func startTimer(_ sender: UIButton) {
+        counter = 30
+//        self.timerLbl.isHidden = false
+        timerObject = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timer), userInfo: nil, repeats: true)
+        random = arc4random_uniform(80) + 30
         print(random)
-        self.directionImage.isHidden = true
-        self.playbutton.isEnabled = false
-        self.clickToPlay.isHidden = true
+//        self.color1.isHidden = true
+        self.playButton1.isEnabled = false
+//        self.playButton2.isHidden = true
+//        self.color2.isHidden = true
         
+    }
+    @IBAction func startTimer2(_ sender: UIButton) {
+        counter2 = 30
+        //        self.timerLbl.isHidden = false
+        timerObject2 = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timer2), userInfo: nil, repeats: true)
+        random2 = arc4random_uniform(80) + 30
+        print(random2)
+//        self.color2.isHidden = true
+        self.playButton2.isEnabled = false
+//        self.playButton1.isHidden = true
+//        self.color1.isHidden = true
     }
     
     func timer() {
         counter += 1
-        self.timerLbl.text = String(counter)
-//        print(timerObject.timeInterval)
-        if counter < 10 && counter > 90 {
+        print(counter)
+        if counter < 30 && counter > 120 {
             timerObject.invalidate()
-            
         }
         else{
-//            print(Int(random))
             if counter == Int(random) {
-                self.directionImage.isHidden =  false
+//                self.color1.isHidden =  false
                 self.timerObject.invalidate()
-                self.playbutton.isEnabled = true
-                self.clickToPlay.isHidden  = false
-                if random % 2 == 0 {
-                    self.directionImage.image  = UIImage(named: "left")
-                }else{
-                    self.directionImage.image  = UIImage(named: "right")
-                }
-                self.timerLbl.isHidden = true
-//                self.view.bringSubview(toFront: self.directionImage)
+                self.playButton1.isEnabled = true
                 
+                if random % 2 == 0 {
+                    self.color1.backgroundColor = UIColor.red
+                }else{
+                    self.color1.backgroundColor = UIColor.blue
+                }
+//                self.color1.isHidden =  false
+//                self.color2.isHidden =  false
+//                self.playButton2.isHidden = false
             }
         }
 
+    }
+    func timer2() {
+        counter2 += 1
+        print(counter2)
+        if counter2 < 30 && counter2 > 120 {
+            timerObject2.invalidate()
+        }
+        else{
+            if counter2 == Int(random2) {
+//                self.color1.isHidden =  false
+                self.timerObject2.invalidate()
+                self.playButton2.isEnabled = true
+                
+                if random2 % 2 == 0 {
+                    self.color2.backgroundColor = UIColor.green
+                }else{
+                    self.color2.backgroundColor = UIColor.yellow
+                }
+//                self.color1.isHidden =  false
+//                self.color2.isHidden =  false
+//                self.playButton1.isHidden = false
+            }
+        }
+        
     }
 
 }
